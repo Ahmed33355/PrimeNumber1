067e194a57108f14e2d619ca2e5eeacfbf0f0e03
Ahmed33355 <119345509+Ahmed33355@users.noreply.github.com>
2022-12-30 01:20:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter start");
+
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter end");
+
+            int end = int.Parse(Console.ReadLine());
+
+            bool prime = true;
+
+
+            for (int x = start; x < end; x++)
+            {
+                for (int y = 2; y < x; y++)
+                {
+                    if (x % y == 0)
+                    {
+
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime == true)
+                {
+                    Console.WriteLine(x);
+                }
+                else
+                {
+                    prime = true;
+                }
+            }
+        }
+    }
+}
+            
+        
